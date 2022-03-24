63ee5cfc53cb4eb744df6cd1abfa288b1b2b576c
Hector Javier Moreno <cytelsystem@gmail.com>
2022-03-23 23:46:29 -0500

    iniciando proyecto

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/app.scss'
+
+
+class App extends React.Component {
+  render() {
+    return <div> <h1>Multicarga</h1></div>
+  }
+}
+
+export default App;
