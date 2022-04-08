af651644876e3d9a2927ea35f0d175c288c49553
Hector Javier Moreno <cytelsystem@gmail.com>
2022-04-07 23:21:06 -0500

    cambios en el css

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -10,27 +10,24 @@ class Servicios extends React.Component {
   render() {
 
     return (
-      <div className="container-fluid">
+      <div className="container-fluid servicios-contenedor-fluid">
         <div className="container container-servicios">
           <small>{configuracion.servicios.nombre1}</small>
           <h2>{configuracion.servicios.nombre2}</h2>
-          {configuracion.servicios.card.map(detalle => (
-            <div className="row" key={detalle.id}>
-              <div className="card col-2">
-
-                <div className="card-imagen" style={{backgroundImage: `url(${require(`../images/${detalle.imagen}`)})`}}>
-                  <h1>hola</h1>
-                </div>
-                <div className="card-text">
-                  <h2>{detalle.titulo}</h2>
-                  <p>{detalle.descripcion}</p>
-                </div>
-                <div className="card-footer"></div>
-              </div>
-
+            <div className="row cont-card">
+              {configuracion.servicios.card.map(detalle => (
+                  <div className="element-card col-2" key={detalle.id}>
+                    <div className="card-imagen" style={{backgroundImage: `url(${require(`../images/${detalle.imagen}`)})`}}>
+                      <h1>hola</h1>
+                    </div>
+                    <div className="element-card-text">
+                      <h2>{detalle.titulo}</h2>
+                      <p>{detalle.descripcion}</p>
+                    </div>
+                    <div className="card-footer"></div>
+                  </div>
+                ))}
             </div>
-
-            ))}
         </div>
       </div>
 
