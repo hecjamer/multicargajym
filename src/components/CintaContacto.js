5d75b71e5d030d88f5789f5bdf4d63b6205bbd6c
Hector Javier Moreno <cytelsystem@gmail.com>
2022-04-15 22:31:59 -0500

    redes Sociales

--- src/components/CintaContacto.js
+++ src/components/CintaContacto.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/CintaContacto.scss';
+import contacto from '../data/configuracion.json'
 
 class CintaContacto extends React.Component {
   render() {
@@ -8,20 +9,20 @@ class CintaContacto extends React.Component {
         <div className="row">
           <div className="col-lg-8 col-sm-12 cintaContacto-Contactenos">
             <div className="row">
-              <div className="col-xxl-2 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href=""><i className="fi-telephone"></i>4666697</a></div>
-              <div className="col-xxl-2 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href=""><i className="fi-telephone"></i>3102508479</a></div>
-              <div className="col-xxl-4 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href="mailto:logistica.bujacargo.jm@gmail.com"><i className="fi-mail"></i>logistica.bujacargo.jm@gmail.com</a></div>
-              <div className="col-xxl-4 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href=""><i className="fi-marker"></i>Calle 10A No 42B - 38 Bogotá</a></div>
+              <div className="col-xxl-2 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href=""><i className="fi-telephone"></i>{contacto.contacto.telefono}</a></div>
+              <div className="col-xxl-2 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href=""><i className="fi-telephone"></i>{contacto.contacto.celular}</a></div>
+              <div className="col-xxl-4 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href="mailto:logistica.bujacargo.jm@gmail.com"><i className="fi-mail"></i>{contacto.contacto.email}</a></div>
+              <div className="col-xxl-4 col-xl-6 col-lg-6 col-md-6 col-ms-12 cintaContacto-Contactenos-element"><a href=""><i className="fi-marker"></i>{contacto.contacto.direccion}</a></div>
             </div>
           </div>
           <div className="col-lg-4 col-sm-12 cintaContacto-redesSociales">
             <div className="row">
-              <div className="col-3"><a href="https://twitter.leonidasesteban" className="social-link twitter"></a></div>
-              <div className="col-3"><a href="https://facebook.com/leonidas.esteban" className="social-link facebook"></a></div>
-              <div className="col-3"><a href="https://instagram.com/leonidasesteban" className="social-link instagram"></a></div>
-              <div className="col-3"><a href="https://instagram.com/leonidasesteban" className="social-link github"></a></div>
-
-            </div>
+              {contacto.contacto.redesSociales.map(redes => {
+                return (
+                  <div className={redes.col} key={redes.id}><a href={redes.href} className={redes.className}></a></div>
+                )
+              })}
+              </div>
 
           </div>
         </div>
--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -21,35 +21,40 @@ class Servicios extends React.Component {
     return (
       <div className="container-fluid servicios-contenedor-fluid">
         <div className="container container-servicios">
-          <small>{configuracion.servicios.nombre1}</small>
-          <h2>{configuracion.servicios.nombre2}</h2>
-            <div className="row cont-card">
-
-                  {configuracion.servicios.card.map(detalle => (
-                    <div className="element-card" key={detalle.id}>
-                      <div className="card-imagen" style={{backgroundImage: `url(${require(`../images/${detalle.imagen}`)})`}}>
-                      </div>
-                      <div className="element-card-text">
-                        <h2>{detalle.titulo}</h2>
-                        <p>{detalle.descripcion}</p>
-                      </div>
-                      <div className="card-footer">
-
-                      {detalle.imgFooter.map(detalle => {
-                        return (
-                        <div key={detalle.id}>
-                          <i className={detalle.icono}></i>
-                          <h6>{detalle.nombre}</h6>
-                        </div>
-                        )
-
-                      })}
-
-                      </div>
-                    </div>
-                  ))}
-
+            <div className="row">
+              <small>{configuracion.servicios.nombre1}</small>
+              <h2>{configuracion.servicios.nombre2}</h2>
             </div>
+
+
+              <div className="row cont-card">
+
+                    {configuracion.servicios.card.map(detalle => (
+                      <div className="element-card" key={detalle.id}>
+                        <div className="card-imagen" style={{backgroundImage: `url(${require(`../images/${detalle.imagen}`)})`}}>
+                        </div>
+                        <div className="element-card-text">
+                          <h2>{detalle.titulo}</h2>
+                          <p>{detalle.descripcion}</p>
+                        </div>
+                        <div className="card-footer">
+
+                        {detalle.imgFooter.map(detalle => {
+                          return (
+                          <div key={detalle.id}>
+                            <i className={detalle.icono}></i>
+                            <h6>{detalle.nombre}</h6>
+                          </div>
+                          )
+
+                        })}
+
+                        </div>
+                      </div>
+                    ))}
+              </div>
+
+
         </div>
       </div>
 
