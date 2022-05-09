55905da8e8904d10624fe8d5cdc7d77d41ef3523
Hector Javier Moreno <cytelsystem@gmail.com>
2022-05-09 17:43:09 -0500

    adicionando cambios y funciona odo

--- src/components/PlaceTime.js
+++ src/components/PlaceTime.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import ReactDOM from 'react-dom';
 import './styles/placeTime.scss'
 import Placetime from '../data/configuracion.json'
 
--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -1,23 +1,9 @@
 import React from 'react'
-// import ReactDOM from 'react-dom';
 import './styles/servicios.scss'
 import configuracion from '../data/configuracion.json'
-import background from '../images/img1.jpg'
-
 
 class Servicios extends React.Component {
-
   render() {
-
-    // configuracion.servicios.card.map(detalle => {
-    //   {detalle.imgFooter.map(imgFooter => {
-    //     console.log(imgFooter.nombre)
-
-    //   })}
-    // })
-
-
-
     return (
       <div className="container-fluid servicios-contenedor-fluid">
         <div className="container container-servicios">
@@ -25,42 +11,33 @@ class Servicios extends React.Component {
               <small>{configuracion.servicios.nombre1}</small>
               <h2>{configuracion.servicios.nombre2}</h2>
             </div>
-
               <div className="row cont-card">
-
                     {configuracion.servicios.card.map(detalle => (
                       <div className="element-card" key={detalle.id}>
-                        <div className="card-imagen" style={{backgroundImage: `url(${require(`../images/${detalle.imagen}`)})`}}>
-                        </div>
+                        <div className="card-imagen" style={{backgroundImage: `url(${require(`../images/${detalle.imagen}`)})`}}></div>
                         <div className="element-card-text">
-                          <h2>{detalle.titulo}</h2>
-                          <p>{detalle.descripcion}</p>
+                          <h2 style={{fontSize: `${detalle.tamanoletraH2}`}}>{detalle.titulo}</h2>
+                          <p style={{fontSize: `${detalle.tamanoletraP}`}}>{detalle.descripcion}</p>
                         </div>
                         <div className="card-footer">
-
-                        {detalle.imgFooter.map(detalle => {
-                          return (
-                          <div key={detalle.id} className={detalle.class}>
-                            <i className={detalle.icono}></i>
-                            <h6>{detalle.nombre}</h6>
-                          </div>
-                          )
-
-                        })}
-
+                          {detalle.imgFooter.map(detalle => {
+                            return (
+                            <div key={detalle.id} className={detalle.class}>
+                              <i className={detalle.icono}></i>
+                              <h6>{detalle.nombre}</h6>
+                            </div>
+                            )
+                          })}
                         </div>
                       </div>
                     ))}
               </div>
-
-
         </div>
       </div>
 
     )
   }
 }
-
 export default Servicios;
 
 
