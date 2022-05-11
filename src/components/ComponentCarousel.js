c57f354766e55d4b92a9fd7743378a09c961d3b5
Hector Javier Moreno <cytelsystem@gmail.com>
2022-05-11 15:40:53 -0500

    arreglos carousel

--- src/components/ComponentCarousel.js
+++ src/components/ComponentCarousel.js
@@ -22,6 +22,19 @@ function ComponentCarousel() {
                   <Carousel.Caption>
                     <h3>{detalle.descripcion}</h3>
                   </Carousel.Caption>
+                  <div className="overlay">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-ms-12 textCarouselRight">
+                          <h1>{configuracion.carousel.tituloPrincipalH1}</h1>
+                          <p>{configuracion.carousel.tituloPrincipalP}</p>
+                          <a href="#" className="btn btn-outline-light">{configuracion.carousel.tituloPrincipalA}</a>
+                          <button type="button" className="btn">{configuracion.carousel.tituloPrincipalBTN}</button>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </Carousel.Item>
               )
             })}
